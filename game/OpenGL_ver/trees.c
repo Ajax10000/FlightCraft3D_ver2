acb7ea83aabb78095d92c43ae22b6b71d80aae43
Ajax <david.de.leon.1000@gmail.com>
2021-01-30 18:37:11 -0600

    In function loadTreeTextures in file trees.c, I removed local variable bytes_per_color, and moved some code to the new function feedIntoTxt1.

--- game/OpenGL_ver/trees.c
+++ game/OpenGL_ver/trees.c
@@ -12,6 +12,11 @@
 #include "terrain.h"         // for getTerrainHeight()
 #include "trees.h"           // for our prototypes
 
+// maximal values, if possible don't exploit maximums
+#define txtWidth2 32
+#define txtHeight2 32
+#define txtWidth3 96
+#define txtHeight3 96
 
 // ####################################################################################################################
 // Function initTrees
@@ -187,30 +192,55 @@ void drawTrees()
 } // end drawTrees function
 
 // ####################################################################################################################
+// Function feedIntoTxt1
+// ####################################################################################################################
+static void feedIntoTxt1(SDL_Surface *image, GLubyte txt1[txtHeight3][txtWidth3][4]) 
+{
+	int i, j;
+	Uint8 red, green, blue, alpha;
+	Uint32 color_to_convert;
+	int txtRES2 = 96;
+
+	for (j = 0; j < txtRES2; j++)
+	{ // vertical
+		for (i = 0; i < txtRES2; i++)
+		{ // horizontal
+			color_to_convert = getPixel(image, i, j);
+			SDL_GetRGBA(color_to_convert, image->format, &red, &green, &blue, &alpha);
+
+			txt1[j][i][0] = (GLubyte)red;
+			txt1[j][i][1] = (GLubyte)green;
+			txt1[j][i][2] = (GLubyte)blue;
+			txt1[j][i][3] = (GLubyte)alpha;
+
+			if (txt1[j][i][0] == 0 && txt1[j][i][1] == 0 && txt1[j][i][2] == 0)
+			{
+				// Add transparency value artificially according to some convention like black => transparent
+				txt1[j][i][3] = 0; // make this pixel totally transparent (alpha = 0) ; opaque is alpha = 255. .
+			}
+
+			if (alpha < 255)
+			{
+				printf("pixel : [%d,%d,%d ,alpha_value: %d]\n", red, green, blue, alpha);
+			}
+		}
+	}
+} // end feedIntoTxt1 function
+
+// ####################################################################################################################
 // Function loadTreeTextures
 // ####################################################################################################################
 void loadTreeTextures()
 {
 	static int texture_generated = 0; // at first call of this function, a 32x32 texture sample will be generated 
-
-	//	Create texture
-// maximal values, if possible don't exploit maximums
-#define txtWidth2 32
-#define txtHeight2 32
-#define txtWidth3 96
-#define txtHeight3 96
-
-	int txtRES2 = 96; // A reasonable texture resolution
 	static GLubyte txt1[txtHeight3][txtWidth3][4];
-
-#ifdef GL_VERSION_1_1
 	static GLuint texName;
-#endif
-
+	int txtRES2 = 96; // A reasonable texture resolution
 	int j, i, k;
 
 	if (texture_generated == 0)
 	{
+		//	Create texture
 		texture_generated = 1;
 
 		for (j = 0; j < txtHeight2; j++)
@@ -231,11 +261,7 @@ void loadTreeTextures()
 		{
 			char filename[100];
 			SDL_Surface *image; // This pointer will reference our bitmap.
-			int bytes_per_color, imhe;
-			Uint8 red, green, blue, alpha;
-			Uint32 color_to_convert;
-
-			bytes_per_color = COLDEPTH / 8;
+			int imhe;
 
 			sprintf(filename, "textures/semitransparent/texture_%i.bmp", texn);
 			printf("TRYING TO OPEN FILE: %s", filename);
@@ -252,39 +278,16 @@ void loadTreeTextures()
 				printf("bitmap RES: %i\n", imhe);
 				SDL_Delay(5);
 
-				// feed into 'the' array used for this
-				for (j = 0; j < txtRES2; j++)
-				{ // vertical
-					for (i = 0; i < txtRES2; i++)
-					{ // horizontal
-						color_to_convert = getPixel(image, i, j);
-						SDL_GetRGBA(color_to_convert, image->format, &red, &green, &blue, &alpha);
-
-						txt1[j][i][0] = (GLubyte)red;
-						txt1[j][i][1] = (GLubyte)green;
-						txt1[j][i][2] = (GLubyte)blue;
-						txt1[j][i][3] = (GLubyte)alpha;
-
-						if (txt1[j][i][0] == 0 && txt1[j][i][1] == 0 && txt1[j][i][2] == 0)
-						{
-							// Add transparency value artificially according to some convention like black => transparent
-							txt1[j][i][3] = 0; // make this pixel totally transparent (alpha = 0) ; opaque is alpha = 255. .
-						}
-
-						if (alpha < 255)
-						{
-							printf("pixel : [%d,%d,%d ,alpha_value: %d]\n", red, green, blue, alpha);
-						}
-					}
-				}
+				// feed into txt1 array
+				feedIntoTxt1(image, txt1);
 
 				// Release the surface
 				SDL_FreeSurface(image);
 
 				texName = gloTexturesAvailable + texn - 1; // VERY CAREFUL!!! NOT OVERWRITE ALREADY OCCUPIED TEXTURES!!
 
-				//---------| TEXTURE PROCESSING |-----THIS PART MUST BE EXECUTED ONLY ONCE!!! 
-				// Otherwise it silently overloads memory at each call
+				// Pass the data on to OpenGL
+
 				glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
 				glGenTextures(1, &texName);
@@ -293,10 +296,10 @@ void loadTreeTextures()
 
 				glBindTexture(GL_TEXTURE_2D, gloTexIds[gloTexturesAvailable + texn - 1]); // [texn-1] because started from 1, be careful
 
-				// what OpenGL should do when texture is magnified GL_NEAREST: non-smoothed texture | GL_LINEAR: smoothed
+				// Indicate what OpenGL should do when texture is magnified (GL_NEAREST => non-smoothed texture; GL_LINEAR => smoothed)
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST ); 
 
-				// ...when texture is miniaturized because far; GL_NEAREST: non-smoothed texture
+				// Indicate what OpenGL should do when texture is miniaturized because far (GL_NEAREST => non-smoothed texture)
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);	
 
 				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, txtRES2, txtRES2, 0, GL_RGBA, GL_UNSIGNED_BYTE, txt1);
@@ -306,7 +309,6 @@ void loadTreeTextures()
 				glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL); // the GL_DECAL option draws texture as is: no color mixing thigs. GL_MODULATE lets mixing.
 
 				glBindTexture(GL_TEXTURE_2D, texName);
-				//--------------------------| END OF TEXTURE LOAD PROCESSING |-------------------------------
 
 				texn++;	// augment count... next texture
 				gloTexturesAvailable++; // ditto but on an extern variable... .
@@ -320,3 +322,4 @@ void loadTreeTextures()
 		} // end while
 	}
 } // end loadTreeTextures function
+
